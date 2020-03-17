d3a1ab5d20e6d6951cc77f850210b8a8c843e4c7
CrosszPai <61010556@kmitl.ac.th>
2020-03-17 18:43:40 +0700

    add swagger api doc and jsdoc support

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 import express from "express";
+import swaggerUi from 'swagger-ui-express'
+
+import { swaggerSpec } from './swagger-doc'
 
 const PORT = process.env.PORT || 8000
 
-// setup Express server
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
+/**
+ * @swagger
+ *  / :
+ *   get:
+ *     description: Test api doc by hello world
+ *     responses: 
+ *        200:
+ *          description: send Hello World text
+ */
 app.get('/', (req, res) => {
     res.send("Hello World")
+})
+
+app.get('/api-doc.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(swaggerSpec)
 })
 
 app.listen(PORT, () => {
