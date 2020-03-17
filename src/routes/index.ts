6a617fc31e10cad7a15f03d535511cee05d3c3fe
CrosszPai <61010556@kmitl.ac.th>
2020-03-18 00:32:06 +0700

    restructure project and remove swagger-jsdoc

---
+++ src/routes/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import { add } from "../utils/add";
+import swaggerUi from 'swagger-ui-express'
+
+import swaggerDoc from './swaggerDoc.json'
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use('/api-docs', swaggerUi.serve)
+
+app.get('/api-docs', swaggerUi.setup(swaggerDoc))
+
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+
+app.get('/', (req, res) => {
+    res.send(add('Hello ', 'World'))
+})
+
+app.post('/', (req, res) => {
+    res.status(200)
+        .json(req.body)
+})
+
+
+export default app
