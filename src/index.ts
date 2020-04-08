d14c4a563ffaf5e1f12a99256eb779f590a37cae
CrosszPai <61010556@kmitl.ac.th>
2020-04-08 13:16:54 +0700

    remove unecessary package and command

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import app from './routes/index'
 import {initMongoose} from './utils/db'
 import mongoose from 'mongoose'
 
-require('dotenv').config()
-
 const PORT = process.env.PORT || 8000
 
 initMongoose()
