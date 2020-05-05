77ca6c465f597aa33fcf11095a5f2578be1980ad
CrosszPai <61010556@kmitl.ac.th>
2020-05-06 01:12:02 +0700

    fix checkOwner

--- src/utils/checkIsOwner.ts
+++ src/utils/checkIsOwner.ts
@@ -1,9 +1,13 @@
-import {getUserById} from "../model/user.model";
-import {JobModel} from "../model/job.model";
+import { getUserById } from "../model/user.model";
+import { JobModel } from "../model/job.model";
+import { Types } from "mongoose";
 
 export const checkIfOwner = async (userId: string, jobId: string): Promise<boolean> => {
-    let user = await getUserById(userId)
+    let user = Types.ObjectId(userId)
     let job = await JobModel.findById(jobId)
         .populate('owner')
-    return user._id === job['owner']._id;
+    console.log(user, job);
+    console.log(user.equals(job['owner']));
+
+    return user.equals(job['owner'])
 }
