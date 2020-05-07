0253186328b7265f1a69a0221f61f4cd638f2771
CrosszPai <43898498+CrosszPai@users.noreply.github.com>
2020-05-07 19:51:40 +0700

    Update db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const initMongoose = async () => {
     try {
-        await mongoose.connect('mongodb+srv://admin:crosszpai13@cluster0-gpf70.mongodb.net/GoodJob?retryWrites=true&w=majority', {
+        await mongoose.connect(process.env.atlas_db, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
