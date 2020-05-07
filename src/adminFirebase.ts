add479559aca488b8702db7ba0a098758fb7532c
CrosszPai <43898498+CrosszPai@users.noreply.github.com>
2020-05-07 19:57:30 +0700

    Update adminFirebase.ts

--- src/adminFirebase.ts
+++ src/adminFirebase.ts
@@ -3,7 +3,7 @@ import admin from 'firebase-admin'
 admin.initializeApp({
   credential: admin.credential.cert({
     clientEmail:process.env.client_email,
-    privateKey:process.env.private_key,
+    privateKey:process.env.private_key.replace(/\\n/g, '\n'),
     projectId:process.env.project_id,
   }),
   databaseURL: process.env.firebase_db,
